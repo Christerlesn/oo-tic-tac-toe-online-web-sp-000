eec15e89880140b9a05a7b7cff5e65af257780e1
Christerle Nounoute <christerlesn@gmail.com>
2020-10-08 16:40:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,4 +2,27 @@ class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
+
+WIN_COMBINATIONS = [
+[0,1,2] #top row
+[3,4,5] #middle row
+[6,7,8] #bottom row
+[0,3,6]#first column
+[1,4,7]#second column
+[2,5,8]#third column
+[0,4,8]#across right
+[2,4,6]#across left
+]
+
+
+
+
+
+
+
+
+
+
+
+  
 end
