6d7ef5f8855219e15f3749f6d7dd22cd2ef6f300
Christerle Nounoute <christerlesn@gmail.com>
2020-10-08 17:28:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -56,7 +56,18 @@ end
 
 def current_player
   turn_count % 2 == 0 ? "X" : "O"
+end
 
+def turn
+input = gets.strip
+index = input_to_index(input)
+char - current_player
+if valid_move?(index)
+  move(index, char)
+  display_board
+else
+  turn
+end
 end
 
 end #end of class TicTacToe
