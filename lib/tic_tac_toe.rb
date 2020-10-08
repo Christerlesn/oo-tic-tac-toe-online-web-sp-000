a64b0f83c736fb60f4ff546557c864ef053b62a7
Christerle Nounoute <christerlesn@gmail.com>
2020-10-08 17:31:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,10 +60,10 @@ end
 
 def turn
 input = gets.strip
-index = input_to_index(input)
-char - current_player
+index = input_to_index(user_input)
+char = current_player
 if valid_move?(index)
-  move(index, char)
+  move(position, char)
   display_board
 else
   turn
