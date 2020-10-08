19c613c4732af36f0a6ea098756dfdeb20aeecc9
Christerle Nounoute <christerlesn@gmail.com>
2020-10-08 16:41:05 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,14 +4,14 @@ class TicTacToe
   end
 
 WIN_COMBINATIONS = [
-[0,1,2] #top row
-[3,4,5] #middle row
-[6,7,8] #bottom row
-[0,3,6]#first column
-[1,4,7]#second column
-[2,5,8]#third column
-[0,4,8]#across right
-[2,4,6]#across left
+[0,1,2], #top row
+[3,4,5], #middle row
+[6,7,8], #bottom row
+[0,3,6], #first column
+[1,4,7], #second column
+[2,5,8], #third column
+[0,4,8], #across right
+[2,4,6] #across left
 ]
 
 
