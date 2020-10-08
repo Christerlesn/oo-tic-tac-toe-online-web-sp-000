04df063908abca04a0f46844504145ecee333692
Christerle Nounoute <christerlesn@gmail.com>
2020-10-08 18:25:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -105,16 +105,16 @@ def winner
   end
 end
 
-# def play
-#   while over? == false
-#     turn
-#   end
-#   if won?
-#     puts "Congratulations #{winner}!"
-#   elsif draw?
-#     puts "Cat's Game!"
-#   end
-# end
+def play
+  while over? == false
+    turn
+  end
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
 
 
 end #end of class TicTacToe
